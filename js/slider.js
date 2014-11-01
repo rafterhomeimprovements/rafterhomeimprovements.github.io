ef7ea4099186a021af96ff61435cb25c68cd24d7
Ryan Billard <ryanagbillard@gmail.com>
2014-11-01 13:56:01 -0400

    Restyled home page, about and contact

--- js/slider.js
+++ js/slider.js
@@ -2,24 +2,16 @@ $(document).ready(function(){
     $('.photo-slider').slick({
       infinite: true,
       slidesToScroll: 1,
+      slidesToShow: 2,
       speed: 1000,
       autoplay: true,
   	  autoplaySpeed: 2000,
       dots: true,
-      // variableWidth: true,
       responsive: [
-      	{
-      		breakpoint:1024,
-      		settings: {
-      			arrows: false,
-      			slidesToShow: 3,
-      		}
-      	},
       	{
       		breakpoint:600,
       		settings: {
       			arrows: false,
-      			slidesToShow: 1
       		}
       	}
       ]
