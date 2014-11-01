25ee1bc69c5305c2cef93edf1dde5b2c252ef882
Ryan Billard <ryanagbillard@gmail.com>
2014-11-01 12:52:23 -0400

    Added fonts

--- _site/js/slider.js
+++ _site/js/slider.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
     $('.photo-slider').slick({
       infinite: true,
-      slidesToShow: 1,
       slidesToScroll: 1,
       speed: 1000,
       autoplay: true,
   	  autoplaySpeed: 2000,
       dots: true,
+      // variableWidth: true,
       responsive: [
+      	{
+      		breakpoint:1024,
+      		settings: {
+      			arrows: false,
+      			slidesToShow: 3,
+      		}
+      	},
       	{
       		breakpoint:600,
       		settings: {
-      			arrows:false,
+      			arrows: false,
+      			slidesToShow: 1
       		}
       	}
       ]
